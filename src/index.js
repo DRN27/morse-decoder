6a6320c023f0bc38b4bcc82418c367223301314a
DRN27 <47753504+DRN27@users.noreply.github.com>
2019-10-11 19:31:52 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result; 
+    let decodingStr = '';
+    for (let i = 0; i < expr.length; i += 10){
+        str = expr.slice(i, i + 10);
+        result = '';
+        for (let j = 0; j < 10; j += 2){
+            let letter = str[j] + str[j + 1];
+            if (letter == 10) {
+                result += '.';
+            } else if (letter == 11){
+                result += '-';
+            } else if (letter == '**'){
+                result = '**';
+                break;
+            }
+        } 
+        if (result in MORSE_TABLE){
+            decodingStr += MORSE_TABLE[result];
+        } else {
+            decodingStr += ' ';
+        }
+    }
+    return decodingStr;
 }
 
 module.exports = {
